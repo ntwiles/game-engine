759fb9fcadae39f39ff84a99d7021e0ecf5b1fff
Nathan Wiles <ntwiles@gmail.com>
2022-12-08 21:27:03 -0500

    Extract pipeline creation to new module.

--- src/graphics/mod.rs
+++ src/graphics/mod.rs
@@ -1,4 +1,5 @@
 pub mod material;
+pub mod pipeline;
 pub mod sorting_layer;
 pub mod sprite;
 pub mod texture;
@@ -10,6 +11,12 @@ use image::{ImageBuffer, Rgba};
 use wgpu::{util::DeviceExt, Sampler, TextureView};
 use wgpu_glyph::{GlyphBrush, GlyphBrushBuilder, Section, Text};
 use winit::window::Window;
+
+use crate::{camera, config::Config, entity, resources, ui::canvas::Canvas};
+
+use self::pipeline::create_sprite_render_pipeline;
+use self::sprite::DrawSprite;
+use self::texture::Texture;
 
 pub struct Graphics {
     camera_bind_group: wgpu::BindGroup,
@@ -28,17 +35,6 @@ pub struct Graphics {
     texture_sampler: Sampler,
     vertex_buffer: wgpu::Buffer,
 }
-
-use super::sprite::DrawSprite;
-
-use crate::{
-    camera,
-    config::Config,
-    entity,
-    graphics::texture::Texture, // this couldn't be imported within super::{} for some reason?
-    resources,
-    ui::canvas::Canvas,
-};
 
 const MAX_ENTITIES: usize = 24000;
 
@@ -142,25 +138,12 @@ impl Graphics {
                 label: Some("camera_bind_group_layout"),
             });
 
-        let sprite_shader = resources::load_string("sprite.wgsl").await.unwrap();
-        let sprite_shader = device.create_shader_module(wgpu::ShaderModuleDescriptor {
-            label: Some("Sprite Shader"),
-            source: wgpu::ShaderSource::Wgsl(sprite_shader.into()),
-        });
-
-        let layout = device.create_pipeline_layout(&wgpu::PipelineLayoutDescriptor {
-            label: Some("Render Pipeline Layout"),
-            bind_group_layouts: &[&texture_bind_group_layout, &camera_bind_group_layout],
-            push_constant_ranges: &[],
-        });
-
-        let render_pipeline = create_render_pipeline(
-            sprite_shader,
-            layout,
-            &[vertex::RenderVertex::desc()],
+        let render_pipeline = create_sprite_render_pipeline(
             &device,
             &surface_config,
-        );
+            &[&texture_bind_group_layout, &camera_bind_group_layout],
+        )
+        .await;
 
         let clear_color = wgpu::Color {
             r: 0.0,
@@ -411,49 +394,6 @@ impl Graphics {
             label: Some(&format!("{name} Bind Group")),
         })
     }
-}
-
-fn create_render_pipeline(
-    shader: wgpu::ShaderModule,
-    layout: wgpu::PipelineLayout,
-    v_buffers: &[wgpu::VertexBufferLayout],
-    device: &wgpu::Device,
-    config: &wgpu::SurfaceConfiguration,
-) -> wgpu::RenderPipeline {
-    device.create_render_pipeline(&wgpu::RenderPipelineDescriptor {
-        label: Some("Render Pipeline"),
-        layout: Some(&layout),
-        vertex: wgpu::VertexState {
-            module: &shader,
-            entry_point: "vs_main",
-            buffers: v_buffers,
-        },
-        fragment: Some(wgpu::FragmentState {
-            module: &shader,
-            entry_point: "fs_main",
-            targets: &[Some(wgpu::ColorTargetState {
-                format: config.format,
-                blend: Some(wgpu::BlendState::ALPHA_BLENDING),
-                write_mask: wgpu::ColorWrites::ALL,
-            })],
-        }),
-        primitive: wgpu::PrimitiveState {
-            topology: wgpu::PrimitiveTopology::TriangleList,
-            strip_index_format: None,
-            front_face: wgpu::FrontFace::Ccw,
-            cull_mode: Some(wgpu::Face::Back),
-            polygon_mode: wgpu::PolygonMode::Fill,
-            unclipped_depth: false,
-            conservative: false,
-        },
-        depth_stencil: None,
-        multisample: wgpu::MultisampleState {
-            count: 1,
-            mask: !0,
-            alpha_to_coverage_enabled: false,
-        },
-        multiview: None,
-    })
 }
 
 fn group_by_sorting_layer(
---
+++ src/graphics/pipeline.rs
@@ -0,0 +1,65 @@
+use crate::resources;
+
+use super::vertex;
+
+fn new(
+    shader: wgpu::ShaderModule,
+    layout: wgpu::PipelineLayout,
+    device: &wgpu::Device,
+    config: &wgpu::SurfaceConfiguration,
+) -> wgpu::RenderPipeline {
+    device.create_render_pipeline(&wgpu::RenderPipelineDescriptor {
+        label: Some("Render Pipeline"),
+        layout: Some(&layout),
+        vertex: wgpu::VertexState {
+            module: &shader,
+            entry_point: "vs_main",
+            buffers: &[vertex::RenderVertex::desc()],
+        },
+        fragment: Some(wgpu::FragmentState {
+            module: &shader,
+            entry_point: "fs_main",
+            targets: &[Some(wgpu::ColorTargetState {
+                format: config.format,
+                blend: Some(wgpu::BlendState::ALPHA_BLENDING),
+                write_mask: wgpu::ColorWrites::ALL,
+            })],
+        }),
+        primitive: wgpu::PrimitiveState {
+            topology: wgpu::PrimitiveTopology::TriangleList,
+            strip_index_format: None,
+            front_face: wgpu::FrontFace::Ccw,
+            cull_mode: Some(wgpu::Face::Back),
+            polygon_mode: wgpu::PolygonMode::Fill,
+            unclipped_depth: false,
+            conservative: false,
+        },
+        depth_stencil: None,
+        multisample: wgpu::MultisampleState {
+            count: 1,
+            mask: !0,
+            alpha_to_coverage_enabled: false,
+        },
+        multiview: None,
+    })
+}
+
+pub async fn create_sprite_render_pipeline(
+    device: &wgpu::Device,
+    config: &wgpu::SurfaceConfiguration,
+    bind_group_layouts: &[&wgpu::BindGroupLayout],
+) -> wgpu::RenderPipeline {
+    let sprite_shader = resources::load_string("sprite.wgsl").await.unwrap();
+    let sprite_shader = device.create_shader_module(wgpu::ShaderModuleDescriptor {
+        label: Some("Sprite Shader"),
+        source: wgpu::ShaderSource::Wgsl(sprite_shader.into()),
+    });
+
+    let layout = device.create_pipeline_layout(&wgpu::PipelineLayoutDescriptor {
+        label: Some("Render Pipeline Layout"),
+        bind_group_layouts,
+        push_constant_ranges: &[],
+    });
+
+    new(sprite_shader, layout, device, config)
+}
